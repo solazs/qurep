05192f27563af43b1763d6b286853213901297d0
solazs <soltesz.balazs.91@gmail.com>
2016-01-08 01:44:17 +0100

    typo in config

--- src/QuReP/ApiBundle/Services/EntityFormBuilder.php
+++ src/QuReP/ApiBundle/Services/EntityFormBuilder.php
@@ -26,7 +26,7 @@ class EntityFormBuilder implements IEntityFormBuilder
         $this->formFactory = $formFactory;
     }
 
-    public function getForm(string $entityClass): Form
+    public function getForm(string $entityClass) : Form
     {
         $properties = $this->getProps($entityClass);
 
