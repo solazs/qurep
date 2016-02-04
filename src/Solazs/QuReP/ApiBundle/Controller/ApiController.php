fdfe19273b68c916de33cd1868c843dfe331b273
solazs <soltesz.balazs.91@gmail.com>
2016-02-05 00:55:32 +0100

    some fixes to get everything working

--- src/Solazs/QuReP/ApiBundle/Controller/ApiController.php
+++ src/Solazs/QuReP/ApiBundle/Controller/ApiController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-class DefaultController extends Controller
+class ApiController extends Controller
 {
     /**
      * @Route("/{apiRoute}", requirements={"apiRoute"=".+"})
